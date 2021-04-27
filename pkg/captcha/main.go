87f1b3978d670c35c494a8e064b3d588eb83d867
wanhello <wankeye@foxmail.com>
2021-04-27 09:39:57 +0800

    redis

--- pkg/captcha/main.go
+++ pkg/captcha/main.go
@@ -19,24 +19,23 @@ type configJsonBody struct {
 	DriverDigit   *base64Captcha.DriverDigit
 }
 
-var store = base64Captcha.DefaultMemStore
-
-func DriverStringFunc() (id, b64s string, err error) {
+func DriverStringFunc(store base64Captcha.Store) (id, b64s string, err error) {
 	e := configJsonBody{}
 	e.Id = uuid.New().String()
 	e.DriverString = base64Captcha.NewDriverString(46, 140, 2, 2, 4,
 		"234567890abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQSTUVWXYZ",
 		&color.RGBA{240, 240, 246, 246}, []string{"wqy-microhei.ttc"})
 	driver := e.DriverString.ConvertFonts()
-	captcha := base64Captcha.NewCaptcha(driver, store)
-	return captcha.Generate()
+	capt := base64Captcha.NewCaptcha(driver, store)
+	return capt.Generate()
 }
 
-func DriverDigitFunc() (id, b64s string, err error) {
+func DriverDigitFunc(store base64Captcha.Store) (id, b64s string, err error) {
 	e := configJsonBody{}
 	e.Id = uuid.New().String()
 	e.DriverDigit = base64Captcha.DefaultDriverDigit
 	driver := e.DriverDigit
-	captcha := base64Captcha.NewCaptcha(driver, store)
-	return captcha.Generate()
+	capt := base64Captcha.NewCaptcha(driver, store)
+
+	return capt.Generate()
 }
