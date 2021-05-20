8d2a288e11ed66301cc9cda2f0d44ab5bb4abc47
wanhello <wankeye@foxmail.com>
2021-05-20 17:53:14 +0800

    docker compose

--- scripts/docker.rabbitmq.sh
+++ scripts/docker.rabbitmq.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-
+docker run -d -p 15691:15691 -p 15692:15692 -p 25672:25672 -p 4369:4369 -p 5671:5671 -p 5672:5672 \
+      -v $PWD/docker/data/rabbitmq:/var/lib/rabbitm \
+      -v $PWD/docker/config/influx:/etc/influxdb2 \
+      -e RABBITMQ_DEFAULT_USER="omgind" \
+      -e RABBITMQ_DEFAULT_PASS="123456" \
+      --name rabbitmq
+      rabbitmq:alpine
