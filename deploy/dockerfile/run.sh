99bba855eeca5ca99cca8b90bef24aae284827db
wanhello <wankeye@foxmail.com>
2021-05-26 17:03:46 +0800

    docker compose

--- deploy/dockerfile/run.sh
+++ deploy/dockerfile/run.sh
@@ -10,7 +10,9 @@ go mod vendor
 #go run /go/omgind/cmd/omgind/main.go web -c /go/omgind/configs/config.toml -m /go/omgind/configs/model.conf --menu /go/omgind/configs/menu.yaml
 
 cd /go/omgind
-echo "----------- "
+
+echo " ----------- "
+
 echo "${PWD}"
 
 air
